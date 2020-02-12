d956b3dd2bef4e6ac7182a40435ba756bd42745c
Carboxide68 <co.dincrod@gmail.com>
2020-02-12 20:27:54 +0100

    Hopefull fixed all weird css bugs

--- js/script.js
+++ js/script.js
@@ -2,39 +2,52 @@ var menuButton = document.getElementById("viewreg");
 var topNav = document.getElementsByClassName("topnav")[0];
 var sideNav = document.getElementsByClassName("sidenav")[0];
 var content = document.getElementsByClassName("content")[0];
-var cued;
+var cued = false;
+
+//Button query
+var btQuery = window.matchMedia("screen and (max-aspect-ratio: 1/1)");
 
 window.addEventListener("resize", function() {
-
     if (!cued) {
-        this.setTimeout(updateCss, 500);
+        setTimeout(fixCss, 500);
         cued = true;
     }
-
 });
 
 window.addEventListener("load", function() {
-    updateCss();
+    fixCss();
 });
 
 menuButton.addEventListener('click', function() {
-    if (sideNav.style.display === "none" || sideNav.style.display === "") {
+    if (sideNav.style.display == "none" || sideNav.style.display == "") {
         sideNav.style.display = "block";
-        sideNav.style.marginTop = topNav.offsetHeight + "px";
     } else {
-        sideNav.style.display = "";
-        sideNav.style.marginTop = "";
+        sideNav.style.display = "none";
     }
 });
 
-function updateCss() {
-    let topHeight = document.getElementById("current").offsetHeight;
-    menuButton.style.height = topHeight + "px";
-    menuButton.style.width = topHeight + "px";
-    content.style.marginLeft = (sideNav.offsetWidth - 2) + "px";
+function fixCss() {
+    //Menu button
+    let current = document.getElementById("current");
+    menuButton.style.width = current.offsetHeight + "px";
+    menuButton.style.height = current.offsetHeight + "px";
+
+    //Fix sideNav for mobile
+    if (btQuery.matches) {
+        sideNav.style.marginTop = topNav.offsetHeight + "px";
+        sideNav.style.display = "none";
+        content.style.marginLeft = "";
+        topNav.style.marginLeft = "";
+        content.style.width = "100%";
+    } else {
+        sideNav.style.marginTop = "";
+        sideNav.style.display = "block";
+        content.style.marginLeft = sideNav.offsetWidth + "px";
+        topNav.style.marginLeft = sideNav.offsetWidth + "px";
+        content.style.width = "calc(100% - " + (sideNav.offsetWidth + "px)");
+    }
+    
     content.style.marginTop = topNav.offsetHeight + "px";
-    topNav.style.marginLeft = (sideNav.offsetWidth - 2) + "px";
-    topNav.style.width = (window.innerWidth - sideNav.offsetWidth) + "px";
-    content.style.width = (window.innerWidth - sideNav.offsetWidth) + "px";
+
     cued = false;
 }
